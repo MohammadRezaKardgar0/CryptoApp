c41d926a85dcce492d7f751f9edb6c253db72fa6
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-22 12:02:47 +0330

    show any data by modal

--- src/components/module/TableCoin.jsx
+++ src/components/module/TableCoin.jsx
@@ -33,8 +33,8 @@ function TableCoin({ coins, isLoading, setChart }) {
 
 export default TableCoin;
 
-const TableRow = ({
-  coin: {
+const TableRow = ({ coin, setChart }) => {
+  const {
     id,
     name,
     image,
@@ -42,14 +42,13 @@ const TableRow = ({
     total_volume,
     current_price,
     price_change_percentage_24h: price_change,
-  },
-  setChart,
-}) => {
+  } = coin;
+
   const showHandler = async () => {
     try {
       const res = await fetch(marketChart(id));
       const json = await res.json();
-      setChart(json);
+      setChart({...json, coin});
     } catch (error) {
       setChart(null);
     }
