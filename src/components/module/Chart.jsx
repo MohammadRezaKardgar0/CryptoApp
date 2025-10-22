4c60ea6bb64afd9d18b03ae917b6fad852f5f1ec
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-22 10:06:47 +0330

    change format data by chart

--- src/components/module/Chart.jsx
+++ src/components/module/Chart.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
+  const [type, setType] = useState("prices");
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={() => setChart(null)}>X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}></div>
     </div>
   );
